01805ee3ee41da2ef55d29e5d845971c5fe5b189
Jason DiCioccio <jd@ods.org>
2019-06-11 21:53:26 -0700

    fix output for more RRD friendly names

--- src/output_formatter.rs
+++ src/output_formatter.rs
@@ -34,41 +34,44 @@ impl OutputFormatter for CricketFormatter {
         let mut iovec: Vec<String> = Vec::new();
 
         for (channel_id, channel) in info.downstream_info.iter() {
-            iovec.push(format!("{}_ds.freq_mhz:{}", channel_id, channel.freq_mhz));
+            iovec.push(format!("{}_ds.freq:{}", channel_id, channel.freq_mhz));
             iovec.push(format!(
-                "{}_ds.power_dbmv:{}",
+                "{}_ds.power:{}",
                 channel_id, channel.power_dbmv
             ));
-            iovec.push(format!("{}_ds.snr_db:{}", channel_id, channel.snr_db));
+            iovec.push(format!("{}_ds.snr:{}", channel_id, channel.snr_db));
             iovec.push(format!(
-                "{}_ds.corrected_errs:{}",
+                "{}_ds.corr_e:{}",
                 channel_id, channel.corrected_errs
+            ));
+            iovec.push(format!(
+                "{}_ds.uncorr_e:{}",
+                channel_id, channel.uncorrected_errs
             ));
 
             let locked = match channel.lock_status.as_str() {
                 "Locked" => 1,
                 _ => 0,
             };
-            iovec.push(format!("{}_ds.lock_status:{}", channel_id, locked));
+            iovec.push(format!("{}_ds.lock_st:{}", channel_id, locked));
 
             match channel.uncorrected_errs_delta {
-                Some(delta) => iovec.push(format!("{}_ds.uncorrected_errs:{}", channel_id, delta)),
+                Some(delta) => iovec.push(format!("{}_ds.uncorr_e_dlt:{}", channel_id, delta)),
                 None => (),
             };
             match channel.corrected_errs_delta {
                 Some(delta) => {
-                    iovec.push(format!("{}_ds.corrected_errs_delta:{}", channel_id, delta))
+                    iovec.push(format!("{}_ds.corr_e_dlt:{}", channel_id, delta))
                 }
                 None => (),
             };
         }
 
         for (channel_id, channel) in info.upstream_info.iter() {
-            iovec.push(format!("{}_us.sym_rate:{}", channel_id, channel.sym_rate));
-            iovec.push(format!("{}_us.freq_mhz:{}", channel_id, channel.freq_mhz));
-            iovec.push(format!("{}_us.sym_rate:{}", channel_id, channel.sym_rate));
+            iovec.push(format!("{}_us.sym_r:{}", channel_id, channel.sym_rate));
+            iovec.push(format!("{}_us.freq:{}", channel_id, channel.freq_mhz));
             iovec.push(format!(
-                "{}_us.power_dbmv:{}",
+                "{}_us.power:{}",
                 channel_id, channel.power_dbmv
             ));
 
@@ -76,7 +79,7 @@ impl OutputFormatter for CricketFormatter {
                 "Locked" => 1,
                 _ => 0,
             };
-            iovec.push(format!("{}_us.lock_status:{}", channel_id, locked));
+            iovec.push(format!("{}_us.lock_st:{}", channel_id, locked));
         }
 
         output = iovec.join(" ");
