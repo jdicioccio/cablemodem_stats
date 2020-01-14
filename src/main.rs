892d58e33ea4a877f99d9395ba1b709e9ab9cff9
Jason DiCioccio <jd@ods.org>
2020-01-13 20:55:15 -0800

    Motorola moved to https, so we are too!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod output_formatter;
 mod response;
-use chttp::http;
+use isahc::prelude::*;
+use isahc::config::SslOption;
 use output_formatter::*;
 use response::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-
-    let req = http::Request::post("http://192.168.100.1/HNAP1/")
+    
+    let req = Request::post("https://192.168.100.1/HNAP1/")
+        .ssl_options(SslOption::DANGER_ACCEPT_INVALID_CERTS)
         .header("Content-Type", "application/json")
 //        .header("Cookie", format!("uid={}; PrivateKey={}", uid, private_key))
 //        .header("HNAP_AUTH", "XXXX 1559973888764")
@@ -15,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .header("Expect", "")
         .body(r#"{"GetMultipleHNAPs":{"GetMotoStatusStartupSequence":"","GetMotoStatusConnectionInfo":"","GetMotoStatusDownstreamChannelInfo":"","GetMotoStatusUpstreamChannelInfo":"","GetMotoLagStatus":""}}"#)?;
 
-    let mut resp = chttp::send(req)?;
+    let mut resp = req.send()?;
 
     //assert!(resp.status().is_success());
 
